a797e13c76c073e4e2bd203f9c69086a29094b53
Raul Fragueiro <raulfragueiro@gmail.com>
2019-02-20 09:54:56 +0100

    Primer commit

---
+++ test/com/cifpcarballeira/katas/FizzBuzz/test/TestFizzBuzz.java
@@ -0,0 +1,26 @@
+package com.cifpcarballeira.katas.FizzBuzz.test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author daw108
+ */
+public class TestFizzBuzz {
+    
+    public TestFizzBuzz() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
